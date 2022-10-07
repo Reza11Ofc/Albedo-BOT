d284b25bc0b92f32c709fa045a14e06dcbbb9f60
Reza11Ofc <113843050+Reza11Ofc@users.noreply.github.com>
2022-10-08 04:06:28 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,15 +1,12 @@
-let handler = async (m, { conn, usedPrefix }) => { 
-      conn.relayMessage(m.chat,  {
-    requestPaymentMessage: {
-      currencyCodeIso4217: 'INR',
-      amount1000: 8888888889,
-      requestFrom: m.sender,
-      noteMessage: {
-      extendedTextMessage: {
-      text: `${global.wm}
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+//let handler = async(m, { conn, text, usedPrefix, command }) => {
+//let pp = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image')
+
+let str = `${global.wm}
 
 ┌─「 Donasi • Pulsa 」
-│ • *Three:* [${global.ppulsa}]
+│ • *Tri:* [${global.ppulsa}]
 ❏────
 
 ┌─「 Donasi • Non Pulsa 」
@@ -17,13 +14,20 @@ let handler = async (m, { conn, usedPrefix }) => {
 │ • *Gopay:* [${global.pgopay}]
 │ • *Ovo:* [${global.povo}]
 │ • *Link Aja:* [${global.plinkaja}]
-❏────`,
-      contextInfo: {
-      externalAdReply: {
-      showAdAttribution: false
-      }}}}}}, {})
-}
-
+❏────`
+let wibu = `https://telegra.ph/file/8cacba9fad7f0d0270234.jpg` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/gamer.s_official",
+    mediaType: "VIDEO",
+    description: "https://www.instagram.com/gamer.s_official", 
+    title: 'EzaBot-MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sig
+  }
+  } }) 
+          }
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
